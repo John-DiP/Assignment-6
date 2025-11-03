5a9632d0c707bfb58eb37fc04eb6965e4cccdf0b
John D. <103323388+John-DiP@users.noreply.github.com>
2025-11-03 10:42:48 -0600

    Add files via upload

---
+++ src/DocumentBST.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+import edu.princeton.cs.algs4.BST;
+
+public class DocumentBST {
+
+	public static void main(String[] args) {
+		LinkedList<BST> BSTDocument = new LinkedList<BST>();
+		
+		
+
+	}
+
+}
