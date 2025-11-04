6c43fcf3eb16a8a5e03f1275ecb89345e4018027
John D. <103323388+John-DiP@users.noreply.github.com>
2025-11-03 23:56:14 -0600

    Add files via upload

--- src/DocumentBST.java
+++ src/DocumentBST.java
@@ -1,11 +1,15 @@
 import java.util.LinkedList;
 import edu.princeton.cs.algs4.BST;
+import java.io.File;
 
 public class DocumentBST {
 
 	public static void main(String[] args) {
 		LinkedList<BST> BSTDocument = new LinkedList<BST>();
 		
+		BST firstDocument = new BST();
+		
+		BSTDocument.add(firstDocument);
 		
 
 	}
